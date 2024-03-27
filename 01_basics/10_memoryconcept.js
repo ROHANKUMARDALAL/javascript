d5438041d66f3bfd7b4af7d433ef595bde045fe0
ROHANKUMARDALAL <140591951+ROHANKUMARDALAL@users.noreply.github.com>
2024-03-27 07:42:18 +0000

    container config done

---
+++ 01_basics/10_memoryconcept.js
@@ -0,0 +1,28 @@
+ //stack memory
+ //Heap memory
+
+
+
+ //stack(Primitvie) in stack  Copy is created .   
+ //heap (non-primitve) in heap reference is given to new data any chajnges makes in all the present
+ // of same data
+ 
+
+let myname="rohanalal"
+let anothername=myname
+ anothername = "Mohit"
+console.log(myname)
+console.log(anothername);
+
+
+let user1 ={//non primitive data type
+    email: "rohankumar.rc@gmail.com",
+    upiid:"abc.axiom"
+}
+let user2=user1
+
+user2.email="abced.@ggg.com"
+
+console.log(user1.email)
+
+console.log(user2.email)
