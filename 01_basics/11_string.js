eb18ad124ae7aeebd7aa5eb04d7cea48f46a5a2c
ROHANKUMARDALAL <140591951+ROHANKUMARDALAL@users.noreply.github.com>
2024-03-27 08:35:30 +0000

    container config done

---
+++ 01_basics/11_string.js
@@ -0,0 +1,20 @@
+const name ="rohan "
+const repo= 50
+
+
+//console.log( name +repo+" 123");//not recommended
+
+console.log(`hello my name is ${name} and my repo is ${repo}`) // string interpullation
+
+const gamename=new String("rohan dalal")
+console.log(gamename)
+console.log(gamename[0])
+console.log(gamename[1])
+console.log(gamename[2])
+console.log(gamename[3])
+console.log(gamename.__proto__)
+console.log(gamename.length)
+console.log(gamename.toLocaleUpperCase())
+/**
+ * various mathods of string describes above
+ */
