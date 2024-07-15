1bb2ff3bb1b58a17534aeba6b19fdb2ce4d315d1
ROHANKUMARDALAL <140591951+ROHANKUMARDALAL@users.noreply.github.com>
2024-07-15 17:06:46 +0000

    container config done

--- 01_basics/04_variables.js
+++ 01_basics/04_variables.js
@@ -1,23 +1,23 @@
 const account_id    =    12345    //const variables cannot be changed further in same code
 
 
-let AccountEmail    =    "rohankumar.12@js.com"
+let accountEmail    =    "rohankumar.12@js.com"
 
 
-var AccountPassowrd =    "123321"   //in now a days var keyword is not used bcz of scope the scope of var is for whole code or for all blocks 
+var accountPassowrd =    "123321"   //in now a days var keyword is not used bcz of scope the scope of var is for whole code or for all blocks 
 
 
 accountCity         =    "Bulandshahr" // not recommended for good programming 
 
-let AccountState   ;  //without initilize the value of variables it will shows as undefined while prinnode 4_variables.jsting  
+let accountState   ;  //without initilize the value of variables it will shows as undefined while printnode 4_variables.jsting  
 
 
 
-AccountEmail       =  "changed email"
-AccountPassowrd    =  "Changed password"
+accountEmail       =  "changed email"
+accountPassowrd    =  "Changed password"
 accountCity        =  "changed city"
 
-console.table([account_id,AccountEmail,AccountPassowrd,accountCity,AccountState]) //console.table is use to print all data variables in table format
+console.table([account_id,accountEmail,accountPassowrd,accountCity,accountState]) //console.table is use to print all data variables in table format
 
 // console.log(AccountPassowrd)
 // console.log(AccountEmail)
