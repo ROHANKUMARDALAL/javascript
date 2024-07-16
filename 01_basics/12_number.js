5de8e7673ab63b3c315d39533a21a997c885417c
ROHANKUMARDALAL <140591951+ROHANKUMARDALAL@users.noreply.github.com>
2024-07-16 07:21:02 +0000

    container config done

--- 01_basics/12_number.js
+++ 01_basics/12_number.js
@@ -26,6 +26,8 @@ console.log(Math.sqrt(144))
 console.log(Math.min(4,9,8,7,1))
 
 console.log(Math.max(4,9,8,7,1))
+
+
 console.log(Math.random())          //always pick a random value lies btw 0 & 1
 console.log((Math.random()*10) +1)
 
