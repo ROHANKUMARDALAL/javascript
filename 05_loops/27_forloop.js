27912dc9c8edff58fdf24f7d9ae96ec18cad287f
ROHANKUMARDALAL <140591951+ROHANKUMARDALAL@users.noreply.github.com>
2024-07-16 22:48:31 +0000

    container config done

---
+++ 05_loops/27_forloop.js
@@ -0,0 +1,10 @@
+//for loop we all are very known to for loop so we skip the code writing only
+
+
+// for(initilization ; condition ; inc/dec)
+// syntax of for loop 
+// nested loop
+
+
+
+// break(use to break the control flow) & continue(only skip the loop for one time) keywords
