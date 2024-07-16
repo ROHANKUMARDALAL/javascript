d857e501610aa9fb2722c80ba4003e2658e1d296
ROHANKUMARDALAL <140591951+ROHANKUMARDALAL@users.noreply.github.com>
2024-07-16 07:15:21 +0000

    conatiner config done

--- 01_basics/12_number.js
+++ 01_basics/12_number.js
@@ -9,11 +9,11 @@ console.log(balance.toLocaleString().length)
 console.log(balance.toFixed(2)) //To print the decimal values 
 
 
-const othernumber=1213.678987
-console.log(othernumber.toPrecision(5)) //always use 1digit more than natural number
+const otherNumber=1213.678987
+console.log(otherNumber.toPrecision(5)) //always use 1digit more than natural number
 
-const hundrd= 10000000
-console.log(hundrd.toLocaleString('en-IN'))
+const hundred= 10000000
+console.log(hundred.toLocaleString('en-IN'))
 
 
 
@@ -24,6 +24,7 @@ console.log(Math)
 console.log(Math.abs(-4))
 console.log(Math.sqrt(144))
 console.log(Math.min(4,9,8,7,1))
+
 console.log(Math.max(4,9,8,7,1))
 console.log(Math.random())          //always pick a random value lies btw 0 & 1
 console.log((Math.random()*10) +1)
